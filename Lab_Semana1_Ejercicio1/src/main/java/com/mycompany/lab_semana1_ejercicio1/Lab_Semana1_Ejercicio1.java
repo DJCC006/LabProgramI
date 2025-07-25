0de77cfe58c6decad38c4441631b694957138f8c
DJCC <david.carcamo2006@gmail.com>
2025-07-25 10:19:10 -0600

    desarrollo de primer ejercicio

--- Lab_Semana1_Ejercicio1/src/main/java/com/mycompany/lab_semana1_ejercicio1/Lab_Semana1_Ejercicio1.java
+++ Lab_Semana1_Ejercicio1/src/main/java/com/mycompany/lab_semana1_ejercicio1/Lab_Semana1_Ejercicio1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.lab_semana1_ejercicio1;
-
+import java.util.Scanner;
 /**
  *
  * @author David
@@ -11,6 +11,23 @@ package com.mycompany.lab_semana1_ejercicio1;
 public class Lab_Semana1_Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String nombre;
+        String puesto;
+        Scanner lea = new Scanner(System.in);
+        lea.useDelimiter("\n");
+        
+        System.out.println("Porfavor ingrese su primer nombre y su primer apellido: ");
+        nombre=lea.next().toUpperCase();
+        System.out.println("\nPorfavor ingrese el puesto que ocupa: ");
+        puesto= lea.next();
+        
+        String mensaje= "\nBienvenido "+nombre+", su puesto es "+puesto;
+        int lengthMensaje=mensaje.length();
+        
+        System.out.println(mensaje);
+        System.out.println("El largo de la cadena es: "+lengthMensaje);
+        
+        
+        
     }
 }
