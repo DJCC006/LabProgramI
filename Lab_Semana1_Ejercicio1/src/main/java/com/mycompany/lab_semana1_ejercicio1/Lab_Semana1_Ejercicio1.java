5df2e3cf5aeba3f8c83d91c1f2a8619c369047d8
DJCC <david.carcamo2006@gmail.com>
2025-07-25 10:10:13 -0600

    Agregado primer ejercicio

---
+++ Lab_Semana1_Ejercicio1/src/main/java/com/mycompany/lab_semana1_ejercicio1/Lab_Semana1_Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_semana1_ejercicio1;
+
+/**
+ *
+ * @author David
+ */
+public class Lab_Semana1_Ejercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
